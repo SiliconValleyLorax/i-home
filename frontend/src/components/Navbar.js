0565667f5800d64d586d702d2662eae85935fe3e
김서연 <sy9815@gmail.com>
2021-07-19 21:51:03 +0900

    refactor: add main home page and fix router

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -66,7 +66,7 @@ function Navbar() {
             </li>
             <li className="nav-btn">
               {button ? (
-                <Link to="/sign-up" className="btn-link">
+                <Link to="/sign-up" className="btn-link" >
                   <Button buttonStyle="btn--outline">Sign Up</Button>
                 </Link>
               ) : (
--- frontend/src/pages/UploadImage.js
+++ frontend/src/pages/UploadImage.js
@@ -6,38 +6,40 @@ import { Link } from "react-router-dom";
 // import Book from './Book'
 
 const UploadImage = () => {
-  // 서버 연결 테스트 코드 - test 버튼으로 동작확인 (지우지 말아주세요ㅠㅠ)
-  // const test = async () => {
-  //   try {
-  //     const response = await axios.post("http://localhost:5000/api/image", {
-  //       image: "https://i.stack.imgur.com/GNhxO.png",
-  //     });
-  //     console.log(response);
-  //   } catch (e) {
-  //     console.log(e);
-  //   }
-  // };
-
-  return (
-    <>
+  const [attachment, setAttachment] = useState(
+    "https://i.stack.imgur.com/GNhxO.png"
+  );
+  // 서버 연결 테스트 코드 - test 버튼으로 동작확인
+  const test = async () => {
+    try {
+      const response = await axios.post("http://localhost:5000/api/image", {
+        image: attachment,
+      });
+      console.log(response);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  
+  return(
+      <>
       <div className="main-container">
-        <div className="container-title">
-          <span>아이가 좋아하는 장난감, 공간 등을 알려주세요</span>
-        </div>
-        <Link to="/CameraSet">
-          <button className="upload-btn camera-btn">
-            <AiFillCamera className="icon-btn photo" />
-            카메라로 등록
-          </button>
-        </Link>
-        <Link to={"/GallerySet"}>
-          <button className="upload-btn gallery-btn">
-            <RiGalleryUploadFill className="icon-btn gallery" />
-            갤러리로 등록
-          </button>
-        </Link>
-        {/* 서버 통신 테스트 위한 버튼입니다! 지우지 말아주세요ㅠㅠ
-        <button onClick={test} className="upload-btn gallery-btn">
+          <div className="container-title">
+            <span>아이가 좋아하는 장난감, 공간 등을 알려주세요</span>
+          </div>
+          <Link to="/CameraSet">
+            <button className="upload-btn camera-btn">
+              <AiFillCamera className="icon-btn photo" />
+              카메라로 등록
+            </button>
+          </Link>
+          <Link to={"/GallerySet"}>
+            <button className="upload-btn gallery-btn">
+              <RiGalleryUploadFill className="icon-btn gallery" />
+              갤러리로 등록
+            </button>
+          </Link>
+          {/* <button onClick={test} className="upload-btn gallery-btn">
             <div>test</div>
           </button> */}
       </div>

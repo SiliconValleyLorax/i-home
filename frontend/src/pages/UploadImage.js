ea0d314277192e792f03fc9bc1804a5002c3f5f7
jiyoung <jiyoung_06@ewhain.net>
2021-07-19 20:33:06 +0900

    chore: refactor
    add home icon, delete unused code...

--- frontend/src/pages/UploadImage.js
+++ frontend/src/pages/UploadImage.js
@@ -1,5 +1,3 @@
-import axios from "axios";
-import { useState } from "react";
 import "../css/UploadImage.css";
 import { AiFillCamera } from "react-icons/ai";
 import { RiGalleryUploadFill } from "react-icons/ri";
@@ -8,47 +6,42 @@ import { Link } from "react-router-dom";
 // import Book from './Book'
 
 const UploadImage = () => {
-  const [attachment, setAttachment] = useState(
-    "https://i.stack.imgur.com/GNhxO.png"
-  );
-
-
-
-  // 서버 연결 테스트 코드 - test 버튼으로 동작확인
-  const test = async () => {
-    try {
-      const response = await axios.post("http://localhost:5000/api/image", {
-        image: attachment,
-      });
-      console.log(response);
-    } catch (e) {
-      console.log(e);
-    }
-  };
+  // 서버 연결 테스트 코드 - test 버튼으로 동작확인 (지우지 말아주세요ㅠㅠ)
+  // const test = async () => {
+  //   try {
+  //     const response = await axios.post("http://localhost:5000/api/image", {
+  //       image: "https://i.stack.imgur.com/GNhxO.png",
+  //     });
+  //     console.log(response);
+  //   } catch (e) {
+  //     console.log(e);
+  //   }
+  // };
 
   return (
-      <>
-        <div className="main-container">
-          <div className="container-title">
-            <span>아이가 좋아하는 장난감, 공간 등을 알려주세요</span>
-          </div>
-          <Link to="/CameraSet">
-            <button className="upload-btn camera-btn">
-              <AiFillCamera className="icon-btn photo" />
-              카메라로 등록
-            </button>
-          </Link>
-          <Link to={"/GallerySet"}>
-            <button className="upload-btn gallery-btn">
-              <RiGalleryUploadFill className="icon-btn gallery" />
-              갤러리로 등록
-            </button>
-          </Link>
-          {/* <button onClick={test} className="upload-btn gallery-btn">
+    <>
+      <div className="main-container">
+        <div className="container-title">
+          <span>아이가 좋아하는 장난감, 공간 등을 알려주세요</span>
+        </div>
+        <Link to="/CameraSet">
+          <button className="upload-btn camera-btn">
+            <AiFillCamera className="icon-btn photo" />
+            카메라로 등록
+          </button>
+        </Link>
+        <Link to={"/GallerySet"}>
+          <button className="upload-btn gallery-btn">
+            <RiGalleryUploadFill className="icon-btn gallery" />
+            갤러리로 등록
+          </button>
+        </Link>
+        {/* 서버 통신 테스트 위한 버튼입니다! 지우지 말아주세요ㅠㅠ
+        <button onClick={test} className="upload-btn gallery-btn">
             <div>test</div>
           </button> */}
-        </div>
-      </>
+      </div>
+    </>
   );
 };
 
